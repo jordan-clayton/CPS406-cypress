533261dd8d0e86ef31723a5539d57385a72ec352
Jordan Clayton <jordan.clayton@torontomu.ca>
2025-04-05 22:13:53 -0400

    Small update to loading to accommodate location_services

--- lib/app/common/loading.dart
+++ lib/app/common/loading.dart
@@ -2,12 +2,13 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../db/impl/postgrest_db_service.dart';
 import '../../db/impl/supabase_client.dart';
+import '../../location/impl/cypress_location_service.dart';
 import '../../login/impl/supabase_login.dart';
 import '../client/client_controller.dart';
 import '../common/constants.dart' as constants;
 
 // NOTE: write other implementation initializers as appropriate
-
+// TODO: locationService Impl.
 /// Initialization function to start the app using supabase implementation
 Future<ClientController> initializeControllerWithSupabase() async {
   // Initialize clients
@@ -18,9 +19,11 @@ Future<ClientController> initializeControllerWithSupabase() async {
   final dbClient = SupabaseImpl.withClient(client: supabase.client);
   final loginService = SupabaseLoginService.withClient(client: supabase.client);
   final dbService = PostgrestDatabaseService(client: dbClient);
+  final locationService = CypressLocationService();
   final controller = ClientController(
       databaseService: dbService,
       loginService: loginService,
+      locationService: locationService,
       loggedIn: loginService.hasSession);
 
   // If the user has a profile with the app, try grabbing the data
